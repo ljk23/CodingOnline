70edbae989d134a2817e1449ec8830de926d2934
Tp5admin <tp5admin@126.com>
2019-04-27 18:29:06 +0800

    4.27后端开发上传

---
+++ test/src/main/java/com/example/login/algorithm/controller/wordCandle.java
@@ -0,0 +1,11 @@
+package com.example.login.algorithm.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/algorium")
+public class wordCandle {
+
+
+}
---
+++ test/src/main/java/com/example/login/submit/controller/wordHandleController.java
@@ -0,0 +1,5 @@
+package com.example.login.submit.controller;
+
+public class wordHandleController {
+
+}
